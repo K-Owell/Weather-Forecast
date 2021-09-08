4f30e9ee81addf2d46a05f0a232c2b79c1453332
Kyle McDowell <k.mcdowellr@gmail.com>
2021-09-07 18:44:36 -0700

    initial API fetch, testing specific weather info logging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,29 @@
 // Variable list
-var cityChoice = document.getElementById("submit")
-
-
+var cityChoice = document.getElementById("submit");
+var userText = document.getElementById("userText");
+var AppId = 'e936b921fafb6dce7a3efcf77c750e6c'
 // API thing with key
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={e936b921fafb6dce7a3efcf77c750e6c}
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
 
 
 
-// Save city input and translate location into coordinates 
+// Save city input 
 
 function cityInput() {
+
     fetch(
-        'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}' + cityInput + '&appid={e936b921fafb6dce7a3efcf77c750e6c}'
+        'https://api.openweathermap.org/data/2.5/weather?q=' + userText.value + '&appid=' + AppId
     )
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response) {
+        console.log(response.weather[0].description)
+    })
+    .catch(function(error) {
+        // find error code
+    })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Listens for Clicky Clicks
